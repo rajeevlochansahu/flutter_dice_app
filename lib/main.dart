6a9333ea778e3168a3ab812ad1dc5a915e5eb591
Rajeev Sahu <rajeev.sahu@ril.com>
2020-08-16 19:12:11 +0530

    StatefulWidget vs StatelessWidget - if you are creating a user interface where the state of the widget isn't going to change then you would use a StatelessWidget. But, if you're creating something which is going to change, especially when it's dependent on user interaction, so a user taps on a button, something changes. Or, a user pulls in some data from the internet, something on the screen updates, then you're going to need to use a StatefulWidget.

--- lib/main.dart
+++ lib/main.dart
@@ -17,20 +17,36 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+/*
+if you are creating a user interface where the state of the widget isn't going to change then you would use a StatelessWidget.
+But, if you're creating something which is going to change, especially when it's dependent on user interaction,
+so a user taps on a button, something changes. Or, a user pulls in some data from the internet, something on the screen updates,
+then you're going to need to use a StatefulWidget.
+ */
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 4;
+  var rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
-    var leftDiceNumber = 1;
-    var rightDiceNumber = 1;
     return Center(
       child: Row(
         children: [
           Expanded(
             child: FlatButton(
-              onPressed: () {
-                print('Left button got pressed');
-              },
               child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = 3;
+                  print(
+                      'Left button got pressed leftDiceNumber = $leftDiceNumber');
+                });
+              },
             ),
           ),
           Expanded(
